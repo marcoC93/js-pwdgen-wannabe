48c93e002b796333859f811e5cf1137dd1aca5b4
Marco <marco.chianese93@gmail.com>
2020-10-12 15:46:19 +0200

    script "completo" (necessarie ottimizzazioni varie e stile)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var  name = prompt("Scrivi il tuo nome") ;
+var  surname = prompt("Scrivi il tuo cognome") ;
+var  fav_color = prompt("Qual'è il tuo colore preferito?") ;
+
+document.getElementById('name').innerHTML = name  ;
+document.getElementById('surname').innerHTML = surname ;
+document.getElementById('fav_color').innerHTML = fav_color ;
