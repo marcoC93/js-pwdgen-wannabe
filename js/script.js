6f584b1f3d2fa07b45cbc42b7bd2f8846685d0c6
Marco <marco.chianese93@gmail.com>
2020-10-12 16:42:23 +0200

    creazione di uno stile..

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+// Inizializzazione variabili
+
 var  name = prompt("Scrivi il tuo nome") ;
 var  surname = prompt("Scrivi il tuo cognome") ;
 var  fav_color = prompt("Qual'è il tuo colore preferito?") ;
 
-document.getElementById('name').innerHTML = name  ;
-document.getElementById('surname').innerHTML = surname ;
-document.getElementById('fav_color').innerHTML = fav_color ;
+
+
+// document.getElementById('name').innerHTML = name  ;
+// document.getElementById('surname').innerHTML = surname ;
+// document.getElementById('fav_color').innerHTML = fav_color ;
+// Cosi funziona ma si può fare di meglio....
+
+//  "generatore" di password
+
+document.getElementById('password').innerHTML = name + surname + fav_color + "20" ;
+
+// è possibile concatenare le variabili per "creare la password"
